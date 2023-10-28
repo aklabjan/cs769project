e40856e94a242d9ed41b77049d44ea7854364886
ANA KLABJAN <aklabjan@instgpu-05.cs.wisc.edu>
2023-10-28 12:52:36 -0500

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-conda create --name goemotion_env
+conda create --name goemotion_env python=3.8.18
 conda activate goemotion_env
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
-conda install transformers -c conda-forge
-conda install attrdict -c conda-forge
-
+conda install --y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install --y  transformers -c conda-forge
+conda install --y attrdict -c conda-forge
+pip install sklearn==0.0
 # for "conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia"
 # please check your cuda version with "nvcc -V", it is very important. And then according to your cuda version to install PyTorch referring the cmd on this page "https://pytorch.org/get-started/locally/"
