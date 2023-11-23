80c9c96eef6f8f4c24d266c3bd93ca16e11197b8
Ana Klabjan <aklabjan@hotmail.com>
2023-11-22 23:28:55 -0600

    restructed backTranslate

--- backTranslation.py
+++ backTranslation.py
@@ -25,36 +25,50 @@ def read_tsv(group, file, delimiter='\t'):
         print(f"Error reading the .tsv file: {e}")
         return None
 
-group = sys.argv[1]
-df = read_tsv(group, 'train.tsv')
-length = len(read_tsv(group,'augmented_train.tsv'))
-df = df[length:]
 
-chunk_size = 50
-chunks = [df[i:i + chunk_size] for i in range(0, len(df), chunk_size)]
+def create_backTranslate(taxonomy):
+    df = read_tsv(taxonomy, 'train.tsv')
+    length = len(read_tsv(taxonomy,'augmented_train.tsv'))
+    df = df[length:]
 
-chunk_number = 0
-for chunk in chunks:
-    count = 0
-    while count < 2:
-        try:
-            output = []
-            for line in chunk['text'].values.tolist():
-                result = trans.translate(line, src='en', tmp = 'ru',sleeping = 0.6)
-                output.append(result.result_text)
-            translated_df = pd.DataFrame({
-            'text': output,
-            'emotion': chunk['emotion'],
-            'labeler': chunk['labeler']
-            })
-            # Specify the path to the existing .tsv file
-            existing_file_path = os.path.join('data',group,'augmented_train.tsv')
+    chunk_size = 50
+    chunks = [df[i:i + chunk_size] for i in range(0, len(df), chunk_size)]
 
-            # Append the new_data DataFrame to the existing .tsv file
-            translated_df.to_csv(existing_file_path, sep='\t', mode='a', header=False, index=False)
-            print(f'completed {chunk_number}') 
-            chunk_number += 1
-            break
-        except Exception as e: 
-            print(f'Error: {e}')
-            count += 1
+    chunk_number = 0
+    for chunk in chunks:
+        count = 0
+        while count < 2:
+            try:
+                output = []
+                for line in chunk['text'].values.tolist():
+                    result = trans.translate(line, src='en', tmp = 'ru',sleeping = 0.6)
+                    output.append(result.result_text)
+                translated_df = pd.DataFrame({
+                'text': output,
+                'emotion': chunk['emotion'],
+                'labeler': chunk['labeler']
+                })
+                # Specify the path to the existing .tsv file
+                existing_file_path = os.path.join('data',taxonomy,'augmented_train.tsv')
+
+                # Append the new_data DataFrame to the existing .tsv file
+                translated_df.to_csv(existing_file_path, sep='\t', mode='a', header=False, index=False)
+                print(f'completed {chunk_number}') 
+                chunk_number += 1
+                break
+            except Exception as e: 
+                print(f'Error: {e}')
+                count += 1
+
+def combine_files(taxonomy):
+    original_df = read_tsv(taxonomy,'train.tsv')
+    translated_df = read_tsv(taxonomy,'augmented_train.tsv')
+    combined_df = original_df.append(translated_df, ignore_index=True)
+    combined_df = combined_df.drop_duplicates()
+    path = os.path.join('data',taxonomy,'augmented_train.tsv')
+    combined_df.to_csv(path, sep='\t', index=False)
+
+if __name__ == "__main__":
+    taxonomy = sys.argv[1]
+    create_backTranslate(taxonomy)
+    #combine_files(taxonomy)
